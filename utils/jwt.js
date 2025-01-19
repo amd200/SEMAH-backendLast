684e7b130d70dbd14c4652e2318222c90f3a20c6
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-19 17:28:47 +0300

    jwt modified

--- utils/jwt.js
+++ utils/jwt.js
@@ -10,16 +10,14 @@ const createJWT = ({ payload }) => {
 const isTokenValid = ({ token }) => jwt.verify(token, process.env.JWT_SECRET);
 
 const attachCookiesToResponse = ({ res, user }) => {
-  const token = createJWT({ payload: user });
+  const accessTokenJWT = createJWT({ payload: { user } });
   const oneDay = 1000 * 60 * 60 * 24;
 
-  res.cookie('token', token, {
+  res.cookie('accessToken', accessTokenJWT, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
-    sameSite: 'none',
-    maxAge: 7 * 24 * 60 * 60 * 1000,
+    expires: new Date(Date.now() + oneDay),
   });
 };
 
