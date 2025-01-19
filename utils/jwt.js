2ed145b7671a612e63fffa8311072ab1c62c9422
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-19 21:23:12 +0300

    deploy

--- utils/jwt.js
+++ utils/jwt.js
@@ -13,7 +13,7 @@ const attachCookiesToResponse = ({ res, user }) => {
   const token = createJWT({ payload: { user } });
   const oneDay = 1000 * 60 * 60 * 24;
 
-  es.cookie('token', token, {
+  res.cookie('token', token, {
     httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
