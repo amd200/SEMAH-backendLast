5f5d1d15396429b2c2a5dffa1cfd3ab727c65e10
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-20 13:40:09 +0300

    commit

--- utils/jwt.js
+++ utils/jwt.js
@@ -14,7 +14,9 @@ const attachCookiesToResponse = ({ res, user }) => {
   const oneDay = 1000 * 60 * 60 * 24;
 
   res.cookie('token', token, {
-    httpOnly: false,
+    domain: 'semah-frontend.vercel.app',
+    path: '/',
+    httpOnly: true,
     expires: new Date(Date.now() + oneDay),
     secure: process.env.NODE_ENV === 'production',
     signed: true,
