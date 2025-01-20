435533866a2b1ac137b5254d5863ea44947d9b38
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-20 14:38:24 +0300

    commit

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ app.use(cookieParser(process.env.JWT_SECRET));
 app.use(
   cors({
     origin: [
-      'https://semah-frontend-6ant.vercel.app',
+      'https://semah-frontend-p4ky.vercel.app',
       'https://semah.basmakhebra.com',
       'http://localhost:5173',
     ],
