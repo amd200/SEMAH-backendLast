38977814eae17f113699c465365fa960aac398c8
Hazem Megahed <hazemsarhan@Hazems-MacBook-Air.local>
2025-01-21 09:44:20 +0300

    leave room added

--- configs/websocketConfig.js
+++ configs/websocketConfig.js
@@ -1,42 +1,46 @@
-import { Server } from "socket.io";
+import { Server } from 'socket.io';
 
 let io; // Declare `io` globally to be accessible across the module
 
 export const initializeWebSocket = (httpServer) => {
   io = new Server(httpServer, {
     cors: {
-      origin: "*", // Allow all origins (you can restrict this to specific origins in production)
-      methods: ["GET", "POST"], // Allowed methods
+      origin: '*', // Allow all origins (you can restrict this to specific origins in production)
+      methods: ['GET', 'POST'], // Allowed methods
     },
   });
 
-  io.on("connection", (socket) => {
-    console.log("A user connected:", socket.id);
+  io.on('connection', (socket) => {
+    console.log('A user connected:', socket.id);
 
     // Handle message sending
-    socket.on("send-message", (data) => {
-      const { chatId, content, sender,createdAt } = data;
-      console.log("send-message", data);
+    socket.on('send-message', (data) => {
+      const { chatId, content, sender, createdAt } = data;
+      console.log('send-message', data);
       // Emit the received message to all clients in the specific chat room
-      io.to(chatId).emit("receive-message", content);
+      io.to(chatId).emit('receive-message', content);
     });
 
     // Handle room joining
-    socket.on("join-room", (chatId) => {
-      console.log("join-room", chatId);
+    socket.on('join-room', (chatId) => {
+      console.log('join-room', chatId);
       socket.join(chatId);
       console.log(`User ${socket.id} joined room: ${chatId}`);
     });
 
-    socket.on("receive-message", (data) => {
+    socket.on('receive-message', (data) => {
       const { content } = data;
-      console.log("receive-message", content);
+      console.log('receive-message', content);
       // Update the chat UI with the new message
     });
 
+    socket.on('leaveRoom', (roomId) => {
+      socket.leave(roomId);
+    });
+
     // Handle disconnection
-    socket.on("disconnect", () => {
-      console.log("A user disconnected:", socket.id);
+    socket.on('disconnect', () => {
+      console.log('A user disconnected:', socket.id);
     });
   });
 
@@ -46,15 +50,15 @@ export const initializeWebSocket = (httpServer) => {
 // Function to notify an employee
 export const notifyEmployee = (employeeId, message) => {
   if (!io) {
-    throw new Error("WebSocket server not initialized");
+    throw new Error('WebSocket server not initialized');
   }
-  io.to(employeeId).emit("notification", message); // Notify the employee
+  io.to(employeeId).emit('notification', message); // Notify the employee
 };
 
 // Function to notify a client
 export const notifyClient = (clientId, message) => {
   if (!io) {
-    throw new Error("WebSocket server not initialized");
+    throw new Error('WebSocket server not initialized');
   }
-  io.to(clientId).emit("notification", message); // Notify the client
+  io.to(clientId).emit('notification', message); // Notify the client
 };
